fa0715841dc0cbbb1846bb5d402d196eabf25cf6
Danielle Bobb <danielle@oakandivymedia.com>
2021-03-28 12:01:10 -0700

    header sections include address, search, hello, returns & orders, cart

--- NOTES.md
+++ NOTES.md
@@ -1,3 +1,5 @@
 <!-- Dependencies:
 FE:
-styled-components -->
+styled-components
+material-ui icons
+ -->
--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import styled from 'styled-components'
+import SearchIcon from '@material-ui/icons/Search'
+import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart'
+import LocationOnIcon from '@material-ui/icons/LocationOn'
 
 function Header() {
 	return (
@@ -8,9 +11,36 @@ function Header() {
 				<img src={'https://i.imgur.com/7I9Was5.png'} />
 			</HeaderLogo>
 			<HeaderOptionAddress>
-				<OptionLineOne>Hello,</OptionLineOne>
-				<OptionLineTwo>Select your address</OptionLineTwo>
+				<LocationOnIcon />
+				<HeaderOption>
+					<OptionLineOne>Hello,</OptionLineOne>
+					<OptionLineTwo>Select your address</OptionLineTwo>
+				</HeaderOption>
 			</HeaderOptionAddress>
+
+			<HeaderSearch>
+				<HeaderSearchInput type='text' />
+				<HeaderSearchIconContainer>
+					<SearchIcon />
+				</HeaderSearchIconContainer>
+			</HeaderSearch>
+
+			<HeaderNavItems>
+				<HeaderOption>
+					<OptionLineOne>Hello, Danielle</OptionLineOne>
+					<OptionLineTwo>Account & Lists</OptionLineTwo>
+				</HeaderOption>
+
+				<HeaderOption>
+					<OptionLineOne>Returns</OptionLineOne>
+					<OptionLineTwo>& Orders</OptionLineTwo>
+				</HeaderOption>
+
+				<HeaderOptionCart>
+					<AddShoppingCartIcon />
+					<CartCount>5</CartCount>
+				</HeaderOptionCart>
+			</HeaderNavItems>
 		</Container>
 	)
 }
@@ -22,7 +52,8 @@ const Container = styled.div`
 	background-color: #0f1111;
 	display: flex;
 	align-items: center;
-	// justify-content: center;
+	justify-content: space-between;
+	color: white;
 `
 
 const HeaderLogo = styled.div`
@@ -32,8 +63,66 @@ const HeaderLogo = styled.div`
 	}
 `
 
-const HeaderOptionAddress = styled.div``
+const HeaderOptionAddress = styled.div`
+	padding-left: 9px;
+	display: flex;
+	align-items: center;
+`
 
 const OptionLineOne = styled.div``
 
-const OptionLineTwo = styled.div``
+const OptionLineTwo = styled.div`
+	font-weight: bold;
+`
+
+const HeaderSearch = styled.div`
+	display: flex;
+	flex-grow: 1;
+	height: 40px;
+	border-radius: 4px;
+	overflow: hidden;
+	margin-left: 4px;
+	background-color: white;
+
+	// adds orange glow box shadow when search input is clicked
+	:focus-within {
+		box-shadow: 0 0 0 3px #f90;
+	}
+`
+
+const HeaderSearchInput = styled.input`
+	flex-grow: 1;
+	border: 0;
+
+	// gets rid of blue outline when search input is clicked
+	:focus {
+		outline: none;
+	}
+`
+
+const HeaderSearchIconContainer = styled.div`
+	background-color: #febd69;
+	width: 45px;
+	color: black;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+`
+const HeaderNavItems = styled.div`
+	display: flex;
+`
+
+const HeaderOption = styled.div`
+	// TROUBLE - Top, Right, Bottom, Left
+	padding: 10px 9px 9px 9px;
+`
+
+const HeaderOptionCart = styled.div`
+	display: flex;
+	align-items: center;
+	padding-right: 9px;
+`
+
+const CartCount = styled.div`
+	padding-left: 4px;
+`
