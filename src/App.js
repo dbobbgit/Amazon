73227c6e21095af2709e84a0d2835d17333d1624
Danielle Bobb <danielle@oakandivymedia.com>
2021-03-28 12:07:09 -0700

    creates Cart component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import "./App.css";
-import Header from "./Header";
-
+import './App.css'
+import Header from './Header'
+import Cart from './Cart'
 
 function App() {
-  return (
-    <div className="App">
-      <Header />
-    </div>
-  );
+	return (
+		<div className='App'>
+			<Header />
+			<Cart />
+		</div>
+	)
 }
 
-export default App;
+export default App
