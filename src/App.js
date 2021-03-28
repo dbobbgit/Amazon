297230d20d4f0819b3f53c9cc55694081b9c0a0e
Danielle Bobb <danielle@oakandivymedia.com>
2021-03-28 12:12:25 -0700

    creates Home component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css'
 import Header from './Header'
 import Cart from './Cart'
-
+import Home from './Home'
 function App() {
 	return (
 		<div className='App'>
 			<Header />
 			<Cart />
+			<Home />
 		</div>
 	)
 }
